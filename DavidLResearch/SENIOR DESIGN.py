b8c42bf3766bab826a377938cddfdb1e08d49fd0
Dlarosag <85146282+Dlarosag@users.noreply.github.com>
2023-11-15 18:13:30 -0500

    Add files via upload

--- DavidLResearch/SENIOR DESIGN.py
+++ DavidLResearch/SENIOR DESIGN.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[9]:
 
 
 import torch
@@ -35,17 +35,21 @@ class CustomAutogradFunction(Function):
         #input, = ctx.saved_tensors
         # You can perform your custom gradient calculation here if needed
         #grad_input = grad_output  # For demonstration, it just passes grad_output as grad_input
+        # Calculate custom gradients (modify as needed)
+        #custom_gradients = input.clone().detach()
+        # Return custom gradients for backpropagation
+        #return custom_gradients * grad_output
         
         input, = ctx.saved_tensors
+        
+        # the gradient of the loss with respect to the output of the custom autograd function is grad_output
+        if(grad_output[0,0].item()!= 0):
+            print("Output Gradient:", grad_output[0,0].item())
+        
+        return grad_output
 
-        # Calculate custom gradients (modify as needed)
-        custom_gradients = input.clone().detach()
 
-        # Return custom gradients for backpropagation
-        return custom_gradients * grad_output
-
-
-# In[2]:
+# In[10]:
 
 
 # Define a simple neural network class
@@ -61,20 +65,20 @@ class NeuralNetwork(nn.Module):
         
         x = CustomAutogradFunction.apply(x)
         x = self.fc1(x)
-        #x = self.custom_layer(x)        
+        x = self.custom_layer(x)        
         x = self.relu(x)
         x = self.fc2(x)
         return x
 
 
-# In[3]:
+# In[11]:
 
 
 # Hyperparameters
 input_size = 784  # MNIST images are 28x28 pixels
 hidden_size = 128
 output_size = 10  # 10 classes (digits 0-9)
-learning_rate = 0.001
+learning_rate = 1
 batch_size = 64
 num_epochs = 10
 
@@ -82,7 +86,7 @@ num_epochs = 10
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
 
-# In[4]:
+# In[12]:
 
 
 train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
@@ -94,7 +98,7 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 criterion = nn.CrossEntropyLoss()
 
 
-# In[5]:
+# In[13]:
 
 
 # Training Loop
@@ -114,25 +118,27 @@ for epoch in range(num_epochs):
         
         if(fc1_weights_neuron1_gradients[0] != 0):
             print("Neuron 0 in Layer 1 at iteration ", i,"Gradient: ", fc1_weights_neuron1_gradients[0], "Weights: ",  fc1_weights_neuron1[0])
+        #print("Default Gradient: ", fc1_weights_neuron1_gradients[0])
         i = i + 1
 print('Training finished.')
 
 
-# In[ ]:
+# In[6]:
 
 
 fc1_weights_neuron1 = model.fc1.weight[0].detach().numpy()
 print(fc1_weights_neuron1[0])
 
 
-# In[ ]:
+# In[7]:
 
 
+#This is the gradient of the loss with respect to the weights of the fc1 layer in the neural network
 fc1_weights_neuron1_gradients = model.fc1.weight.grad[0].detach().numpy()
 print(fc1_weights_neuron1_gradients[0])
 
 
-# In[ ]:
+# In[8]:
 
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
@@ -162,6 +168,12 @@ with torch.no_grad():
 # Calculate and print the total accuracy
 accuracy = 100 * correct / total
 print(f'Total Accuracy: {accuracy:.2f}%')
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
